a3c64e5177dbb605a747c5af320d383a9beb6513
Tapiocaman <acore@uci.edu>
2014-04-23 21:04:32 -0700

    Added placeholders

---
+++ P2/jacobi.cpp
@@ -0,0 +1,14 @@
+#include "imath.h"
+#include "integer.h"
+
+integer J(integer& x, integer& y{
+	if(x == 1){
+		return 1;
+	}
+	//else if(x is even){
+		//return J(x/2,y)*((-1)^(((x-1)*(y-1))/4))
+	//}
+
+	//else
+	//return J( y(mod x), x)*((-1)^(((x-1)*(y-1))/4))
+}
