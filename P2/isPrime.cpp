b91498b93f6eeb1eb5349b9593ed5cb9076db500
Tapiocaman <acore@uci.edu>
2014-05-03 21:47:13 -0700

    made some progress

--- P2/isPrime.cpp
+++ P2/isPrime.cpp
@@ -1,5 +1,6 @@
 #include "integer.h"
 #include "imath.h"
+#include "randGen.h"
 #include <cassert>
 namespace imath
 {
@@ -14,10 +15,9 @@ bool isPrime(const integer& x, const integer& k)
 	}
 	else {
 		//get numeric value of k
-
-
 		for(integer i=0; i<k; i = i+(integer)1){
-			//if(GCD(random value , x) && J(random value, x) is the same as (random value^((x-1)/2))(modN)
+			integer random = dshrandom(temp/(integer)3);
+			//if(GCD(random , x) && J(random value, x) is the same as (random value^((x-1)/2))(modN)
 				//continue loop
 			//else return false
 		}
